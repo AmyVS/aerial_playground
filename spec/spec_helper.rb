664e3b1f2c15589b3c7d5ad105e0b5809413602e
Amy Vaillancourt-Sals <amydecat@yahoo.com>
2014-08-14 16:41:28 -0700

    add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'pg'
+require 'rspec'
+
+require 'student'
+require 'teacher'
+
+DB = PG.connect({:dbname => 'aerial_playground_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM students *;")
+    DB.exec("DELETE FROM teachers *;")
+  end
+end
+
