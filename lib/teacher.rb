7616cc9f05b439f3a7b6db9be04b222ca5dc38c4
amyvs <amydecat@yahoo.com>
2014-08-15 15:35:38 -0700

    add method for teachers to update their apparatus.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -8,4 +8,9 @@ class Teacher < Ringmaster
     @apparatus = attributes['apparatus']
   end
 
+  def update_apparatus(new_apparatus)
+    results = DB.exec("UPDATE teachers SET apparatus = '#{new_apparatus}' WHERE id = #{@id} RETURNING apparatus;")
+    @apparatus = results.first['apparatus']
+  end
+
 end
