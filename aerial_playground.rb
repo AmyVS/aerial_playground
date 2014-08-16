7436fd89b5fb29705e1e48273a3facaeaff7f365
amyvs <amydecat@yahoo.com>
2014-08-15 17:01:11 -0700

    add database information to the UI

--- aerial_playground.rb
+++ aerial_playground.rb
@@ -1,15 +1,20 @@
+require 'pg'
+
+require './lib/ringmaster'
 require './lib/teacher'
 require './lib/student'
 
+DB = PG.connect(:dbname => 'aerial_playground')
+
 def welcome
-  puts "*" * 32
+  puts "*" * 33
   puts "Welcome to the Aerial Playground!"
-  puts "*" * 32
+  puts "*" * 33
   puts "\n"
   puts "Enter [t] if you are a teacher, [s] if you are a student, or [x] to exit."
   user_choice = gets.chomp
 
-  case who
+  case user_choice
   when 't'
     teacher_menu
   when 's'
