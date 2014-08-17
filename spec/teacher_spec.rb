b5168ad0555b5fd8f616e455d1860bdd9cc50933
amyvs <amydecat@yahoo.com>
2014-08-17 13:59:11 -0700

    stick to one 'assign_to' method.

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -52,7 +52,7 @@ describe Teacher do
     test_student.save
     test_teacher = Teacher.new({'name' => 'Daniela', 'apparatus' => 'static trapeze'})
     test_teacher.save
-    test_teacher.assign_to(test_student)
+    test_student.assign_to(test_teacher)
     test_teacher.unenroll(test_student)
     expect(test_teacher.students).to eq []
   end
@@ -62,7 +62,7 @@ describe Teacher do
     test_teacher.save
     test_student = Student.new({'name' => 'Lee'})
     test_student.save
-    test_teacher.assign_to(test_student)
+    test_student.assign_to(test_teacher)
     test_student.delete
     expect(test_student.teachers).to eq []
   end
